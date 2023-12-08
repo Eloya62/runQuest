20753381d60d4dd630d6b735da6038e71ae4e279
Antoine <antoine.pelet@u-bordeaux.com>
2023-12-08 10:54:28 +0100

    update script and uxf

--- bd/scripts_py/main.py
+++ bd/scripts_py/main.py
@@ -5,10 +5,26 @@ import sys
 from bs4 import BeautifulSoup
 import re
 
+months = {
+    'janvier': 1,
+    'février': 2,
+    'mars': 3,
+    'avril': 4,
+    'mai': 5,
+    'juin': 6,
+    'juillet': 7,
+    'août': 8,
+    'septembre': 9,
+    'octobre': 10,
+    'novembre': 11,
+    'décembre': 12
+}
+
 # Define the base URLs
 future_events_url = "https://www.protiming.fr/Runnings/liste/page:{}/sort:Running.date/direction:asc?url=Runnings/liste"
 past_events_url = "https://www.protiming.fr/Results/liste/page:{}/sort:Running.date/direction:asc?url=Runnings/liste"
 course_info_url = "https://www.protiming.fr/Runnings/detail/{}"
+
 
 def get_course_id_for_page(page_number):
     """
@@ -31,4 +47,9 @@ def get_course_id_for_page(page_number):
 
     return course_ids
 
-print(get_course_id_for_page(12))
+course_ids = []
+course_ids_temp = get_course_id_for_page(0)
+while len(course_ids_temp) > 0:
+    course_ids += course_ids_temp
+    course_ids_temp = get_course_id_for_page(len(course_ids))
+print(course_ids)
