14485ea1f80edcaabc94656b8f4dbd6ff4d0fde7
Tanguy Decabrat <103762138+TDecabrat@users.noreply.github.com>
2023-12-11 10:42:54 +0100

    Updated db - All courses obtained.

--- bd/scripts_py/main.py
+++ bd/scripts_py/main.py
@@ -8,23 +8,19 @@ address = "https://ke1fuw1ty5-dsn.algolia.net/1/indexes/*/queries?x-algolia-agen
 json_data = {
     "requests": [
         {
-            "indexName": "entire_races",#"production_races",
+            "indexName": "production_races",#"production_races",
             "params": 'facets=["_tags","eventMonths", "raceDate", "raceDescription", "raceDistance","raceElevationGain","raceElevationLoss","raceItraPoints","region.lvl0"]',
             'facetFilters': ["region.lvl1:Europe > France"],
             "highlightPostTag": "</ais-highlight-0000000000>",
             "highlightPreTag": "</ais-highlight-0000000000>",
-            "hitsPerPage": 20,
+            "hitsPerPage": 1000,
             "maxValuesPerFacet": 100,
             "query": "",
-            "page": "49",
+            "page": "0",
+            "numericFilters": '["raceDate>=1719014400"]'
         }
     ]
 }
-
-req = requests.post(address, json=json_data)
-print(req)
-req = json.loads(req.text)
-n = 3
 
 def get_info_for_course_from_req(req, n):
     premiere_course = req['results'][0]['hits'][n]
@@ -33,7 +29,7 @@ def get_info_for_course_from_req(req, n):
     raceDistance = premiere_course['raceDistance']
     raceElevationGain = premiere_course['raceElevationGain']
     raceElevationLoss = premiere_course['raceElevationLoss']
-    raceDescription = premiere_course['raceDescription']
+    raceDescription = premiere_course['eventSubtitle']
     if 'lvl3' not in premiere_course['region'] and 'lvl4' not in premiere_course['region']:
         region = premiere_course['region']['lvl2'].split(' > ')[2]
         department = premiere_course['region']['lvl2'].split(' > ')[3]
@@ -46,6 +42,8 @@ def get_info_for_course_from_req(req, n):
     localisation = premiere_course['_geoloc']
     lat = localisation['lat']
     lng = localisation['lng']
+    race_distances = premiere_course['raceDistanceVariants']
+    race_name = premiere_course['raceTitle']
     return [
         eventName,
         date,
@@ -56,20 +54,34 @@ def get_info_for_course_from_req(req, n):
         region,
         department,
         lat,
-        lng
+        lng,
+        race_distances,
+        race_name
     ]
 
-holder = []
-columns = ['nom', 'date', 'distance', 'dplus', 'dminus', 'description', 'region', 'departement', 'lat', 'lng']
-for i in range(len(req['results'][0]['hits'])):
-    holder.append(get_info_for_course_from_req(req, i))
-df = pd.DataFrame.from_records(holder, columns=columns)
+req = requests.post(address, json=json_data)
+req = json.loads(req.text)
 
-print(df)
+while len(req['results'][0]['hits']) > 10:
+    holder = []
+    columns = ['nom', 'date', 'distance', 'dplus', 'dminus', 'description', 'region', 'departement', 'lat', 'lng', 'raceDistances', 'raceName']
+    for i in range(len(req['results'][0]['hits'])):
+        holder.append(get_info_for_course_from_req(req, i))
 
-#df.to_csv('courses.csv', index=False)
+    df = pd.DataFrame.from_records(holder, columns=columns)
 
+    # Append to courses.csv
+    df.to_csv('courses.csv', mode='a', header=columns, index=False)
 
-# Saving into a .json the request return
-with open('courses2.json', 'w') as outfile:
-    json.dump(req, outfile)
+    # Saving into a .json the request return
+    with open('courses2.json', 'w') as outfile:
+        json.dump(req, outfile)
+    
+    # Update the request
+    json_data['requests'][0]['numericFilters'] = '["raceDate>=' + str(req['results'][0]['hits'][-1]['raceDate']) + '"]'
+    req = requests.post(address, json=json_data)
+    req = json.loads(req.text)
+
+print(req['results'][0]['hits'][0].keys())
+#Save the last request in a .json, but with all the columns
+print(req['results'][0]['hits'][0])
