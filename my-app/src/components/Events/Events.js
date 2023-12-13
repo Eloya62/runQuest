542f83c472982631076dd8d154c5bbceceabd523
Pauline Cerello <pauline.cerello@etu.u-bordeaux.fr>
2023-12-13 11:46:23 +0100

    css homepage

---
+++ my-app/src/components/Events/Events.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../../General.css";
+import Event from "../Event/Event";
+
+function Events() {
+  return (
+    <div className="">
+      <Event></Event>
+    </div>
+  );
+}
+
+export default Events;
