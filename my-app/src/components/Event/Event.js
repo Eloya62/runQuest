789fd9862320174786628af74dff949194b73f77
Pauline Cerello <pauline.cerello@etu.u-bordeaux.fr>
2023-12-15 14:28:34 +0100

    feature: link between back and front

--- my-app/src/components/Event/Event.js
+++ my-app/src/components/Event/Event.js
@@ -6,17 +6,32 @@ function Event(props) {
   const propsEvent = props.props;
   console.log(propsEvent.races);
 
+  function formatDate(date) {
+    var dateBegin = new Date(date);
+    var day = dateBegin.getDate();
+    var month = dateBegin.getMonth() + 1;
+    var year = dateBegin.getFullYear();
+    return day + "/" + month + "/" + year;
+  }
+
+  console.log("id ",propsEvent.id_event);
+
+  const toLink = {
+    pathname : "/event-details",
+    search: "?id=" + propsEvent.id_event,
+  }
+
   return (
-    <Link to={{ pathname: "/event-details"}} state={{ title: propsEvent.title, dateBegin: propsEvent.dateBegin, location: propsEvent.location, description: propsEvent.description, races: propsEvent.races }}>
+    <Link to={toLink} state={{ title: propsEvent.title, dateBegin: propsEvent.dateBegin, ville: propsEvent.ville, description: propsEvent.description, departement: propsEvent.departement }}>
       <div className="event shadow-md p-2 m-2 bg-white" >
         
         <div className="m-4">
           <div className="event__info flex justify-between">
             <div>
-              <h2 className="event__title"><b>{propsEvent.title}</b></h2>
-              <p className="event__location">{propsEvent.location}</p>
+              <h2 className="event__title"><b>{propsEvent.title?propsEvent.title:""}</b></h2>
+              <p className="event__location">{(propsEvent.ville?propsEvent.ville:"") + (propsEvent.departement?("("+ propsEvent.departement+ ")"):"")}</p>
             </div>
-            <p className="event__date">{propsEvent.dateBegin}</p>
+            <p className="event__date">{propsEvent.dateBegin?formatDate(propsEvent.dateBegin):""}</p>
           </div>
         </div>
         
--- my-app/src/components/Race/Race.js
+++ my-app/src/components/Race/Race.js
@@ -4,34 +4,37 @@ import { Link } from "react-router-dom";
 
 function Race(props) {
   const propsRace = props.props;
+  console.log("RACE ",propsRace);
+
+  function formatDate(date) {
+    var dateBegin = new Date(date);
+    var day = dateBegin.getDate();
+    var month = dateBegin.getMonth() + 1;
+    var year = dateBegin.getFullYear();
+    return day + "/" + month + "/" + year;
+  }
+
+  const toLink = {
+    pathname : "/race-details",
+    search: "?id=" + propsRace.id_course,
+  }
 
   return (
     <Link
-      to={{ pathname: "/race-details" }}
-      state={{
-        title: propsRace.title,
-        date: propsRace.date,
-        location: propsRace.location,
-        distance: propsRace.distance,
-        positiveElevation: propsRace.positiveElevation,
-        negativeElevation: propsRace.negativeElevation,
-        description: propsRace.description,
-        longitude: propsRace.longitude,
-        latitude: propsRace.latitude,
-      }}
+    to={toLink} state={{ title: propsRace.nom_course, dateBegin: propsRace.date_course, region: propsRace.region, description: propsRace.descr, departement: propsRace.departement }}
     >
       <div className="race shadow-md p-2 m-2 bg-white">
         <div className="m-4">
           <div className="race__info flex justify-between">
             <div>
               <h2 className="race__title">
-                <b>{propsRace.title}</b>
+                <b>{propsRace.nom_course}</b>
               </h2>
-              <p className="race__location">{propsRace.location}</p>
+              <p className="race__location">{propsRace.departement?propsRace.departement:""} {propsRace.region?propsRace.region:""}</p>
             </div>
             <div>
-              <p className="race__date">{propsRace.date}</p>
-              <p className="race__distance">{propsRace.distance}</p>
+              <p className="race__date">{propsRace.date_course?formatDate(propsRace.date_course):""}</p>
+              <p className="race__distance">{propsRace.distance_m+ " m"}</p>
             </div>
           </div>
         </div>
