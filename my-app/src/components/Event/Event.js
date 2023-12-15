f42e1f5d4517faa370e465ce4a712969ecaadd17
Pauline Cerello <pauline.cerello@etu.u-bordeaux.fr>
2023-12-15 15:37:20 +0100

    feature: add button del and modif

--- my-app/src/components/Event/Event.js
+++ my-app/src/components/Event/Event.js
@@ -2,9 +2,11 @@ import React from "react";
 import "../../General.css";
 import { Link } from "react-router-dom";
 
+
 function Event(props) {
   const propsEvent = props.props;
   console.log(propsEvent.races);
+  var role = localStorage.getItem("role");
 
   function formatDate(date) {
     var dateBegin = new Date(date);
@@ -31,7 +33,11 @@ function Event(props) {
               <h2 className="event__title"><b>{propsEvent.title?propsEvent.title:""}</b></h2>
               <p className="event__location">{(propsEvent.ville?propsEvent.ville:"") + (propsEvent.departement?("("+ propsEvent.departement+ ")"):"")}</p>
             </div>
-            <p className="event__date">{propsEvent.dateBegin?formatDate(propsEvent.dateBegin):""}</p>
+            <div class="flex flex-end space-x-4">
+              <p className="event__date">{propsEvent.dateBegin?formatDate(propsEvent.dateBegin):""}</p>
+            
+              {(role==="Administrateur") ? <div class="flex flex-col space-y-2"><img src="circle-xmark-regular.svg" class="h-8" alt="cross" /><img src="gear-solid.svg" class="h-8" alt="gear" /></div>: ""}
+            </div>
           </div>
         </div>
         
